fd98800e6f2610611446c53de168d0bc4af1126f
eduqg <eduardoqgomes@gmail.com>
2020-01-18 15:11:09 -0300

    Map to mobile

--- 10_devradar/mobile/src/pages/Main.js
+++ 10_devradar/mobile/src/pages/Main.js
@@ -1,28 +1,92 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 // import { Container } from './styles';
 
-export default function Main() {
+export default function Main({ navigation }) {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04
+        });
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Main!
-      </Text>
-    </View>
+    <MapView
+      style={styles.map}
+      initialRegion={currentRegion}
+    >
+      <Marker coordinate={{ latitude: -15.8478025, longitude: -47.9713124 }}>
+        <Image style={styles.avatar} source={{ uri: 'https://robohash.org/ola' }} />
+
+        <Callout onPress={() => {
+          navigation.navigate('Profile', { github_username: 'diego3g' })
+        }} >
+          <View style={styles.callout}>
+            <Text style={styles.devName}>Diego Fernandes</Text>
+            <Text style={styles.devBio}>Super Programador!</Text>
+            <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
+          </View>
+        </Callout>
+      </Marker>
+
+    </MapView>
   );
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
+  map: {
+    flex: 1
   },
-  title: {
+
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#fff'
+  },
+
+  callout: {
+    width: 260,
+  },
+
+  devName: {
     fontWeight: 'bold',
-    fontSize: 32,
-    color: '#fff',
-  }
+    fontSize: 16,
+  },
+
+  devBio: {
+    color: '#666',
+    marginTop: 5,
+  },
+
+  devTechs: {
+    marginTop: 5,
+  },
 });
